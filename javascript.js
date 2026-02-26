d855ec95cf39e27b0d07ad9a2b92b9203fc2bfef
Jean-Romaric <romaric.akpoue@epitech.eu>
2026-02-26 02:15:41 -0700

    .

---
+++ javascript.js
@@ -0,0 +1,38 @@
+  document.addEventListener('DOMContentLoaded', function() {
+     let elemnt_footer = document.querySelector('footer');
+     let  div_footer = elemnt_footer.querySelector('div');
+    
+
+  async function fetchData() {
+  try {
+    const reponse = await fetch('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?limit=25'); 
+      console.log(reponse);
+    // Vérifiez si la requête a réussi => Response { type: "cors", url: "https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?limit=6", redirected: false, status: 200, ok: true, statusText: "", headers: Headers(6), body: ReadableStream, bodyUsed: false }
+    if (!reponse.ok) {
+      throw new Error('La réponse du réseau n\'était pas OK');
+    }
+
+    const data = await reponse.json(); // Convertissez la réponse en JSON
+    console.log(data); // Affichez les données dans la console => Object { total_count: 69202, results: (6) […] }
+    console.log(data.results); // Utilisez les données récupérées => Array(6) [ {…}, {…}, {…}, {…}, {…}, {…} ]
+
+    console.log(data.results.nom_etablissement); // Affiche "undefined" car results est un tableau
+    console.log(data.results[0].nom_etablissement); // Affiche le nom du premier établissement
+
+
+    data.results.forEach(item=> {
+     console.log(item); 
+
+      let p_element = document.createElement('p');
+      p_element.textContent = item.nom_etablissement;
+      div_footer.appendChild(p_element);
+      p_element.style.color = "green";
+    });
+
+
+  } catch (error) {
+    console.error('Il y a eu un problème avec l\'opération fetch :', error);
+  }
+}
+fetchData();
+});
---
+++ script.js
@@ -0,0 +1,31 @@
+
+const  div = document.querySelector("div")
+
+
+async function fetchData() {
+  try {
+    const response = await fetch('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?limit=25');
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+
+    data.results.forEach(item=> {
+     console.log(item); 
+
+      let p_element = document.createElement('p');
+      p_element.textContent = item.nom_etablissement;
+      div.appendChild(p_element);
+      p_element.style.color = "green";
+    });
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+fetchData();
