807d37f5a7095846c11478bbee556e926ecc4c2a
Jean-Romaric <romaric.akpoue@epitech.eu>
2026-02-26 05:50:44 -0700

    -

--- javascript.js
+++ javascript.js
@@ -5,14 +5,14 @@
 
   async function fetchData() {
   try {
-    const reponse = await fetch('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?limit=25'); 
-      console.log(reponse);
+    const rat = await fetch('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?limit=25'); // Remplacez par votre URL d'API
+      console.log(rat);
     // Vérifiez si la requête a réussi => Response { type: "cors", url: "https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?limit=6", redirected: false, status: 200, ok: true, statusText: "", headers: Headers(6), body: ReadableStream, bodyUsed: false }
-    if (!reponse.ok) {
+    if (!rat.ok) {
       throw new Error('La réponse du réseau n\'était pas OK');
     }
 
-    const data = await reponse.json(); // Convertissez la réponse en JSON
+    const data = await rat.json(); // Convertissez la réponse en JSON
     console.log(data); // Affichez les données dans la console => Object { total_count: 69202, results: (6) […] }
     console.log(data.results); // Utilisez les données récupérées => Array(6) [ {…}, {…}, {…}, {…}, {…}, {…} ]
 
@@ -26,13 +26,16 @@
       let p_element = document.createElement('p');
       p_element.textContent = item.nom_etablissement;
       div_footer.appendChild(p_element);
-      p_element.style.color = "green";
-    });
+      p_element.style.color = "green";});
 
 
   } catch (error) {
     console.error('Il y a eu un problème avec l\'opération fetch :', error);
   }
 }
-fetchData();
+// Appelez la fonction pour exécuter la requête
+  fetchData();
+  console.log('S execute apres fetchData mais avant la fin de fetchData' );
+
+
 });
--- script.js
+++ script.js
@@ -1,31 +1,35 @@
+  document.addEventListener('DOMContentLoaded', function() {
+     let elemnt_footer = document.querySelector('footer');
+     let  div_footer = elemnt_footer.querySelector('div');
+    
 
-const  div = document.querySelector("div")
-
-
-async function fetchData() {
+  async function fetchData() {
   try {
-    const response = await fetch('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?limit=25');
-
+    const response = await fetch('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-annuaire-education/records?limit=25'); // Remplacez par votre URL d'API
+      console.log(response);
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error('La réponse du réseau n\'était pas OK');
     }
 
-    const data = await response.json();
+    const data = await response.json(); 
 
-    console.log(data,);
 
     data.results.forEach(item=> {
      console.log(item); 
 
       let p_element = document.createElement('p');
       p_element.textContent = item.nom_etablissement;
-      div.appendChild(p_element);
-      p_element.style.color = "green";
-    });
+      div_footer.appendChild(p_element);
+      p_element.style.color = "green";});
+
 
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Il y a eu un problème avec l\'opération fetch :', error);
   }
 }
+// Appelez la fonction pour exécuter la requête
+  fetchData();
+  console.log('S execute apres fetchData mais avant la fin de fetchData' );
 
-fetchData();
+
+});
